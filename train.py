ffcc4a24da1257d7461aae4eac3dfd340494e646
Jonas Triki <trikijonas@gmail.com>
2020-07-08 14:41:12 +0200

    Enable dynamic GPU memory

--- train.py
+++ train.py
@@ -350,6 +350,17 @@ def main(args=None):
     # use multiple GPUs if specified
     multi_gpu = args.gpu and len(args.gpu.split(',')) > 1
     if multi_gpu:
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            try:
+                for gpu in gpus:
+                    # Prevent TensorFlow from allocating all memory of all GPUs:
+                    tf.config.experimental.set_memory_growth(gpu, True)
+                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+            except RuntimeError as e:
+                print(e)
+        
         gpus_str = [f'/gpu:{i}' for i in args.gpu.split(',')]
         strategy = MirroredStrategy(devices=gpus_str)
         print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
